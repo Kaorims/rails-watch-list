139cf31058ee22063727a25deac6f6f2ef9021d9
Kaori Martin <kaori.martin21@gmail.com>
2023-01-07 14:26:05 +0100

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+TOP_RATED = 'https://tmdb.lewagon.com/movie/top_rated'
+html_file = URI.open(TOP_RATED).read
+sample = JSON.parse(html_file)
+
+
+movies = sample["results"]
+
+movies.each do |movie|
+  puts movie["title"]
+  # Movie.create(title: movie["title"], overview: "overview", poster_url: "poster_path", rating "vote_average":)
+end
